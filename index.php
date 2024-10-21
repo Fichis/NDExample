5290a39ae0f28b73e4a6ada968fe7113755a1cde
Adolfo <adolfoinsti@gmail.com>
2024-10-22 01:25:35 +0200

    add suma

--- index.php
+++ index.php
@@ -1,11 +1,34 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>NDExample</title>
 </head>
+
 <body>
     <h1>Estructura basica</h1>
+    <?php
+        /*
+            Function: suma
+            Suma dos números
+
+            Parameters:
+                a - Primer número
+                b - Segundo número
+            
+            Returns:
+                El resultado de la suma
+            
+            See Also:
+                
+        */
+        function suma($a, $b)
+        {
+            return $a + $b;
+        }
+    ?>
 </body>
+
 </html>
